8c820608fb8d5b8b5d008b7de24b9ca334b66d2c
SoundRevise <jacobtaylorspam@gmail.com>
2021-10-20 17:56:51 -0700

    Created EQ Parameters

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -28,9 +28,9 @@ void SimpleEQAudioProcessorEditor::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.setColour (juce::Colours::darkorange);
+    g.setFont (45.0f);
+    g.drawFittedText ("STFU EQ", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void SimpleEQAudioProcessorEditor::resized()
